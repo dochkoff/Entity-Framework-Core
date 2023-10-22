3ba7277025ae0bff64ddb2c327c8095bbdf88868
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-10-22 21:52:57 +0300

    T04-P03

---
+++ Entity Framework Core - 2023/T04_ADO.NET_Exercises/P03_MinionNames/P03_MinionNames.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using P03_MinionNames;
+
+internal class Program
+{
+
+    const string connectionString = "Server=.;Database=T04_MinionsDB;User Id=sa;Password=N45tejvWcK;TrustServerCertificate=True;";
+    static SqlConnection? connection;
+
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            await GetOrderedMinionsByVillainId(1);
+        }
+        finally
+        {
+            connection?.Close();
+        }
+    }
+
+    static async Task GetOrderedMinionsByVillainId(int id)
+    {
+        using SqlCommand command = new SqlCommand(SqlQueries.getVillainById, connection);
+        command.Parameters.AddWithValue("@Id", id);
+        var result = await command.ExecuteScalarAsync();
+
+        if (result is null)
+        {
+            await Console.Out.WriteLineAsync($"No villain with ID {id} exists in the database.");
+        }
+        else
+        {
+            await Console.Out.WriteLineAsync($"Vallain: {result}");
+
+            using SqlCommand commandGetMinionData = new SqlCommand(SqlQueries.GetOrderedMinionsByVillainId, connection);
+            commandGetMinionData.Parameters.AddWithValue("@Id", id);
+
+            var minionsRearder = await commandGetMinionData.ExecuteReaderAsync();
+
+            while (await minionsRearder.ReadAsync())
+            {
+                await Console.Out.WriteLineAsync($"{minionsRearder["RowNum"]}. " + $"{minionsRearder["Name"]} {minionsRearder["Age"]}");
+            }
+
+        }
+    }
+}
---
+++ Entity Framework Core - 2023/T04_ADO.NET_Exercises/P03_MinionNames/SqlQueries.cs
@@ -0,0 +1,25 @@
+﻿namespace P03_MinionNames
+{
+    public static class SqlQueries
+    {
+        public const string getVillainsWithNumberOfminions =
+            @"SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                FROM Villains AS v 
+                JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+            GROUP BY v.Id, v.Name 
+              HAVING COUNT(mv.VillainId) > 3 
+            ORDER BY COUNT(mv.VillainId)";
+
+        public const string getVillainById =
+            @"SELECT Name FROM Villains WHERE Id = @Id";
+
+        public const string GetOrderedMinionsByVillainId =
+            @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) AS RowNum,
+                        m.Name, 
+                        m.Age
+                FROM MinionsVillains AS mv
+                JOIN Minions As m ON mv.MinionId = m.Id
+               WHERE mv.VillainId = @Id
+            ORDER BY m.Name";
+    }
+}
