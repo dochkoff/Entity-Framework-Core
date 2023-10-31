b2aedcb6fe22e0af1935dd0d693fc831faa78a60
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-10-31 22:42:22 +0200

    T08-P02

---
+++ Entity Framework Core - 2023/T08_EntityFrameworkIntroduction_Exercise/P03_EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+
+        }
+
+
+    }
+}
