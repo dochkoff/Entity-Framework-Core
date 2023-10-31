a0754637aaa241ebecbbde0d3168f96280130428
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-10-31 22:43:16 +0200

    T08-P03

--- Entity Framework Core - 2023/T08_EntityFrameworkIntroduction_Exercise/P03_EmployeesFullInformation/StartUp.cs
+++ Entity Framework Core - 2023/T08_EntityFrameworkIntroduction_Exercise/P03_EmployeesFullInformation/StartUp.cs
@@ -9,9 +9,26 @@ namespace SoftUni
         {
             SoftUniContext context = new SoftUniContext();
 
+            Console.WriteLine(GetEmployeesFullInformation(context));
 
         }
 
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .ToList();
+
+            string result = string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:F2}"));
+            return result;
+        }
 
     }
 }
