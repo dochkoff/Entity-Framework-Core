5b370829c016ebe8e74e84d5078c9165a60682bd
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-11-07 22:37:32 +0200

    T12-P01

---
+++ Entity Framework Core - 2023/T12_LINQ_Exercise/MusicHub/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = "Server=.;Database=MusicHub;User Id=sa;Password=N45tejvWcK;TrustServerCertificate=True;";
+    }
+}
