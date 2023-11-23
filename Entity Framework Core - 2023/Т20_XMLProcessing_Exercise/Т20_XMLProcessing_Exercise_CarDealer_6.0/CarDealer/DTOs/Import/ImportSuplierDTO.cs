3941de25885d789d1a615567c5bb13616197f31f
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-11-23 21:58:35 +0200

    T20-P09

---
+++ Entity Framework Core - 2023/Т20_XMLProcessing_Exercise/Т20_XMLProcessing_Exercise_CarDealer_6.0/CarDealer/DTOs/Import/ImportSuplierDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Supplier")]
+    public class ImportSuplierDTO
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
+
--- Entity Framework Core - 2023/Т20_XMLProcessing_Exercise/Т20_XMLProcessing_Exercise_CarDealer_6.0/CarDealer/StartUp.cs
+++ Entity Framework Core - 2023/Т20_XMLProcessing_Exercise/Т20_XMLProcessing_Exercise_CarDealer_6.0/CarDealer/StartUp.cs
@@ -1,10 +1,50 @@
-﻿namespace CarDealer
+﻿using System.Data;
+using System.Xml.Serialization;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            CarDealerContext context = new CarDealerContext();
 
+            //P09
+            string inputSuppliersXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(context, inputSuppliersXml));
+        }
+
+        private static Mapper GetMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddProfile<CarDealerProfile>());
+            return new Mapper(cfg);
+        }
+
+        //P09
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            //1.Create XML serializer
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSuplierDTO[]), new XmlRootAttribute("Suppliers"));
+
+            //2.
+            using var reader = new StringReader(inputXml);
+            ImportSuplierDTO[] importSuplierDTOs = (ImportSuplierDTO[])xmlSerializer.Deserialize(reader);
+
+            //3.
+            var mapper = GetMapper();
+            Supplier[] suppliers = mapper.Map<Supplier[]>(importSuplierDTOs);
+
+            //4. Add to EF context
+            context.AddRange(suppliers);
+
+            //5 Commut DB changes
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
         }
     }
 }
