52b5145927d6edeb3b5539e082d3edc9073ed196
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-11-25 13:18:07 +0200

    T20-P01

--- Entity Framework Core - 2023/Т20_XMLProcessing_Exercise/Т20_XMLProcessing_Exercise_ProdutShop_6.0/ProductShop/StartUp.cs
+++ Entity Framework Core - 2023/Т20_XMLProcessing_Exercise/Т20_XMLProcessing_Exercise_ProdutShop_6.0/ProductShop/StartUp.cs
@@ -1,10 +1,43 @@
-﻿namespace ProductShop
+﻿using System.Xml.Serialization;
+using AutoMapper;
+using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+
+namespace ProductShop
 {
     public class StartUp
     {
         public static void Main()
         {
+            ProductShopContext context = new();
 
+            //P01
+            string inputUsersXml = File.ReadAllText("../../../Datasets/users.xml");
+            Console.WriteLine(ImportUsers(context, inputUsersXml));
+        }
+
+        private static Mapper GetMapper()
+        {
+            var cfg = new MapperConfiguration(c => c.AddProfile<ProductShopProfile>());
+            return new Mapper(cfg);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new(typeof(ImportUserDTO[]), new XmlRootAttribute("Users"));
+
+            using StringReader reader = new(inputXml);
+            ImportUserDTO[] importUserDTOs = (ImportUserDTO[])xmlSerializer.Deserialize(reader);
+
+            var mapper = GetMapper();
+
+            User[] users = mapper.Map<User[]>(importUserDTOs);
+
+            context.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
         }
     }
 }
